c95833b7ceadcdd5161d8cfc3069c92c8a266f21
arden LEE <ardensdevspace@gmail.com>
2025-08-18 16:30:36 +0900

    [FIX] changed username page: formbutton, onNextTap, dispose

--- lib/features/authentication/widgets/username_screen.dart
+++ lib/features/authentication/widgets/username_screen.dart
@@ -1,9 +1,49 @@
 import 'package:flutter/material.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
+import 'package:tiktok_clone/features/authentication/widgets/email_screen.dart';
+import 'package:tiktok_clone/features/authentication/widgets/form_button.dart';
 
-class UserNamScreen extends StatelessWidget {
+class UserNamScreen extends StatefulWidget {
   const UserNamScreen({super.key});
+
+  @override
+  State<UserNamScreen> createState() =>
+      _UserNamScreenState();
+}
+
+class _UserNamScreenState extends State<UserNamScreen> {
+  final TextEditingController _usernameController =
+      TextEditingController();
+
+  String _username = "";
+
+  @override
+  void initState() {
+    super.initState();
+
+    _usernameController.addListener(() {
+      setState(() {
+        _username = _usernameController.text;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    // 데이터가 없으면 메모리도 지워주도록 dispose
+    _usernameController.dispose();
+    super.dispose();
+  }
+
+  void _onNextTap() {
+    if (_username.isEmpty) return;
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (context) => const EmailScreen(),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,6 +74,7 @@ class UserNamScreen extends StatelessWidget {
             ),
             Gaps.v16,
             TextField(
+              controller: _usernameController,
               decoration: InputDecoration(
                 hintText: "Username",
                 enabledBorder: UnderlineInputBorder(
@@ -50,23 +91,10 @@ class UserNamScreen extends StatelessWidget {
               cursorColor: Theme.of(context).primaryColor,
             ),
             Gaps.v16,
-            FractionallySizedBox(
-              widthFactor: 1,
-              child: Container(
-                padding: const EdgeInsets.symmetric(
-                  vertical: Sizes.size12,
-                ),
-                decoration: BoxDecoration(
-                  color: Theme.of(context).primaryColor,
-                ),
-                child: const Text(
-                  'Next',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w600,
-                  ),
-                ),
+            GestureDetector(
+              onTap: _onNextTap,
+              child: FormButton(
+                disabled: _username.isEmpty,
               ),
             ),
           ],
