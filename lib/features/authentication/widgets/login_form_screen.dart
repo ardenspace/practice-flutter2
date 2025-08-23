7cd4f545bd4dd2b30d4fe8967f06433567354b35
arden LEE <ardensdevspace@gmail.com>
2025-08-23 22:12:42 +0900

    Add an interest screen

--- lib/features/authentication/widgets/login_form_screen.dart
+++ lib/features/authentication/widgets/login_form_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 import 'package:tiktok_clone/features/authentication/widgets/form_button.dart';
+import 'package:tiktok_clone/features/authentication/widgets/onboarding/interests_screen.dart';
 
 class LoginFormScreen extends StatefulWidget {
   const LoginFormScreen({super.key});
@@ -21,7 +22,11 @@ class _LoginFormScreenState extends State<LoginFormScreen> {
     if (_formKey.currentState != null) {
       if (_formKey.currentState!.validate()) {
         _formKey.currentState!.save();
-        print(formData);
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => const InterestsScreen(),
+          ),
+        );
       }
     }
   }
@@ -44,6 +49,9 @@ class _LoginFormScreenState extends State<LoginFormScreen> {
                   hintText: "Email",
                 ),
                 validator: (value) {
+                  if (value != null && value.isEmpty) {
+                    return 'Email is required';
+                  }
                   return null;
                 },
                 onSaved: (newValue) {
@@ -59,6 +67,9 @@ class _LoginFormScreenState extends State<LoginFormScreen> {
                 ),
                 obscureText: true,
                 validator: (value) {
+                  if (value != null && value.isEmpty) {
+                    return 'Password is required';
+                  }
                   return null;
                 },
                 onSaved: (newValue) {
