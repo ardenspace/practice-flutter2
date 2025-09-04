261e0145be1f46e99ffc454dd2476180b1c9d3a9
arden LEE <ardensdevspace@gmail.com>
2025-09-05 06:35:55 +0900

    feat: install video player, initialize video and autoplay

--- lib/features/videos/video_post.dart
+++ lib/features/videos/video_post.dart
@@ -1,28 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:video_player/video_player.dart';
 
-class VidoeoPost extends StatefulWidget {
-  const VidoeoPost({super.key});
+class VideoPost extends StatefulWidget {
+  final Function onVideoFinished;
+  const VideoPost({
+    super.key,
+    required this.onVideoFinished,
+  });
 
   @override
-  State<VidoeoPost> createState() => _VidoeoPostState();
+  State<VideoPost> createState() => _VideoPostState();
 }
 
-class _VidoeoPostState extends State<VidoeoPost> {
+class _VideoPostState extends State<VideoPost> {
   final VideoPlayerController _videoPlayerController =
       VideoPlayerController.asset(
         "assets/videos/test_video.mp4",
       );
 
+  void _onVideoChanged() {
+    if (_videoPlayerController.value.isInitialized) {
+      final duration =
+          _videoPlayerController.value.duration;
+      final position =
+          _videoPlayerController.value.position;
+
+      if (duration.inMilliseconds > 0 &&
+          position.inMilliseconds > 0 &&
+          position.inMilliseconds >=
+              duration.inMilliseconds * 0.95) {
+        widget.onVideoFinished();
+      }
+    }
+  }
+
   void _initVideoPlayer() async {
-    await _videoPlayerController.initialize();
-    _videoPlayerController.play();
+    try {
+      await _videoPlayerController.initialize();
+      _videoPlayerController.play();
+      setState(() {});
+      _videoPlayerController.addListener(_onVideoChanged);
+    } catch (e) {
+      debugPrint('Video initialization error: $e');
+    }
   }
 
   @override
   void initState() {
     super.initState();
     _initVideoPlayer();
+  }
+
+  @override
+  void dispose() {
+    _videoPlayerController.dispose();
+    super.dispose();
   }
 
   @override
--- lib/features/videos/video_timeline_screen.dart
+++ lib/features/videos/video_timeline_screen.dart
@@ -13,32 +13,30 @@ class _VideoTimelineScreenState
     extends State<VideoTimelineScreen> {
   int _itemCount = 4;
   final PageController _pageController = PageController();
+  final Duration _scrollDuration = const Duration(
+    milliseconds: 300,
+  );
+  final Curve _scrollCurve = Curves.easeInOut;
 
-  List<Color> colors = [
-    Colors.amber,
-    Colors.blue,
-    Colors.pink,
-    Colors.deepPurple,
-  ];
-
-  void _onChangePages(int page) {
-    _pageController.animateToPage(
-      page,
-      duration: const Duration(microseconds: 150),
-      curve: Curves.linear,
-    );
-
+  void _onPageChanged(int page) {
     if (page == _itemCount - 1) {
       setState(() {
         _itemCount = _itemCount + 4;
-        colors.addAll([
-          Colors.amber,
-          Colors.blue,
-          Colors.pink,
-          Colors.deepPurple,
-        ]);
       });
     }
+  }
+
+  void _onVideoFinished() {
+    _pageController.nextPage(
+      duration: _scrollDuration,
+      curve: _scrollCurve,
+    );
+  }
+
+  @override
+  void dispose() {
+    _pageController.dispose();
+    super.dispose();
   }
 
   @override
@@ -46,9 +44,10 @@ class _VideoTimelineScreenState
     return PageView.builder(
       controller: _pageController,
       scrollDirection: Axis.vertical,
-      onPageChanged: _onChangePages,
+      onPageChanged: _onPageChanged,
       itemCount: _itemCount,
-      itemBuilder: (context, index) => const VidoeoPost(),
+      itemBuilder: (context, index) =>
+          VideoPost(onVideoFinished: _onVideoFinished),
     );
   }
 }
