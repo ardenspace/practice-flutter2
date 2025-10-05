ea67cb128b1eca55e820b387c8976685ef922c4d
arden LEE <ardensdevspace@gmail.com>
2025-10-05 16:06:22 +0900

    fix: edit a text field in a chat screen

--- lib/features/inbox/chat_detail_screen.dart
+++ lib/features/inbox/chat_detail_screen.dart
@@ -24,6 +24,7 @@ class _ChatDetailScreenState
             clipBehavior: Clip.none,
             children: [
               const CircleAvatar(
+                radius: Sizes.size20,
                 foregroundImage: NetworkImage(
                   "https://avatars.githubusercontent.com/u/202112113?s=400&u=d44fdf9d52f4e677b0dec4786da0cfde6bed80e7&v=4",
                 ),
@@ -68,6 +69,128 @@ class _ChatDetailScreenState
             ],
           ),
         ),
+      ),
+      body: Stack(
+        children: [
+          ListView.separated(
+            padding: const EdgeInsets.symmetric(
+              vertical: Sizes.size10,
+              horizontal: Sizes.size14,
+            ),
+            itemBuilder: (context, index) {
+              final isMine = index % 2 == 0;
+              return Row(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: isMine
+                    ? MainAxisAlignment.end
+                    : MainAxisAlignment.start,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(
+                      Sizes.size10,
+                    ),
+                    decoration: BoxDecoration(
+                      color: isMine
+                          ? Colors.blue
+                          : Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.only(
+                        topLeft: const Radius.circular(
+                          Sizes.size20,
+                        ),
+                        topRight: const Radius.circular(
+                          Sizes.size20,
+                        ),
+                        bottomLeft: Radius.circular(
+                          isMine
+                              ? Sizes.size20
+                              : Sizes.size5,
+                        ),
+                        bottomRight: Radius.circular(
+                          isMine
+                              ? Sizes.size5
+                              : Sizes.size20,
+                        ),
+                      ),
+                    ),
+                    child: const Text(
+                      "this is a message",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: Sizes.size16,
+                      ),
+                    ),
+                  ),
+                ],
+              );
+            },
+            separatorBuilder: (context, index) => Gaps.v10,
+            itemCount: 10,
+          ),
+          Positioned(
+            bottom: 0,
+            width: MediaQuery.of(context).size.width,
+            child: Container(
+              color: Colors.grey.shade50,
+              padding: const EdgeInsets.symmetric(
+                horizontal: Sizes.size14,
+                vertical: Sizes.size20,
+              ),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        contentPadding:
+                            const EdgeInsets.only(
+                              left: Sizes.size16,
+                            ),
+                        hintText: "Send a message...",
+                        hintStyle: TextStyle(
+                          color: Colors.grey.shade500,
+                        ),
+                        suffixIcon: const Padding(
+                          padding: EdgeInsets.all(
+                            Sizes.size16,
+                          ),
+                          child: FaIcon(
+                            FontAwesomeIcons.faceSmile,
+                            color: Colors.black,
+                            size: Sizes.size24,
+                          ),
+                        ),
+                        border: const OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(Sizes.size20),
+                          ),
+                          borderSide: BorderSide.none,
+                        ),
+                        filled: true,
+                        fillColor: Colors.white,
+                      ),
+                    ),
+                  ),
+                  Gaps.h10,
+                  Container(
+                    padding: const EdgeInsets.all(
+                      Sizes.size12,
+                    ),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(
+                        Sizes.size48,
+                      ),
+                      color: Colors.grey.shade300,
+                    ),
+                    child: const FaIcon(
+                      FontAwesomeIcons.solidPaperPlane,
+                      color: Colors.white,
+                      size: Sizes.size20,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
