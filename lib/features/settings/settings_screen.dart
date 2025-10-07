a8e49a8f7556260e0dac18e77b8262684dececa6
arden LEE <ardensdevspace@gmail.com>
2025-10-07 14:01:33 +0900

    feat: studied DatePicker and CheckBoxListTile

--- lib/features/settings/settings_screen.dart
+++ lib/features/settings/settings_screen.dart
@@ -1,18 +1,76 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
+
+  @override
+  State<SettingsScreen> createState() =>
+      _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  bool _notification = false;
+
+  void _onNotificationsChanged(bool? newValue) {
+    if (newValue == null) return;
+    setState(() {
+      _notification = newValue;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Settings")),
-      body: const Column(
+      body: ListView(
         children: [
-          CupertinoActivityIndicator(radius: 40),
-          CircularProgressIndicator(),
-          CircularProgressIndicator.adaptive(),
+          Switch(
+            value: _notification,
+            onChanged: _onNotificationsChanged,
+          ),
+          CheckboxListTile(
+            checkColor: Colors.white,
+            activeColor: Colors.black,
+            value: _notification,
+            onChanged: _onNotificationsChanged,
+            title: const Text("Enable notifications"),
+          ),
+          ListTile(
+            onTap: () async {
+              final date = await showDatePicker(
+                context: context,
+                firstDate: DateTime(1980),
+                lastDate: DateTime(2030),
+              );
+              print(date);
+
+              final time = await showTimePicker(
+                context: context,
+                initialTime: TimeOfDay.now(),
+              );
+              print(time);
+
+              final booking = await showDateRangePicker(
+                context: context,
+                firstDate: DateTime(1980),
+                lastDate: DateTime(2030),
+                builder: (context, child) {
+                  return Theme(
+                    data: ThemeData(
+                      appBarTheme: const AppBarTheme(
+                        foregroundColor: Colors.white,
+                        backgroundColor: Colors.black,
+                      ),
+                    ),
+                    child: child!,
+                  );
+                },
+              );
+              print(booking);
+            },
+            title: const Text("What is your birthday?"),
+          ),
+          const AboutListTile(),
         ],
       ),
     );
