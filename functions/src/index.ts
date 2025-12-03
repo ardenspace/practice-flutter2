538cf18de37fbeb9941b63275880bddc8c76338c
Arden Lee <ardensdevspace@gmail.com>
2025-12-03 20:00:07 +0900

    feat: 영상 올리면 firestore에 users 안에 collection이 생성됨

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -27,8 +27,21 @@ export const onVideoCreated = onDocumentCreated(
       `/tmp/${snapshot.id}.jpg`, // 임시 tmp 폴더에 저장
     ]);
     const storage = admin.storage();
-    await storage.bucket().upload(`/tmp/${snapshot.id}.jpg`, {
+    const [file, _] = await storage.bucket().upload(`/tmp/${snapshot.id}.jpg`, {
       destination: `thumbnails/${snapshot.id}.jpg`,
     });
+    await file.makePublic();
+    await snapshot.ref.update({ thumbnailUrl: file.publicUrl() });
+
+    const db = admin.firestore();
+    await db
+      .collection("users")
+      .doc(video.creatorUid)
+      .collection("videos")
+      .doc(snapshot.id)
+      .set({
+        thumbnailUrl: file.publicUrl(),
+        videoId: snapshot.id,
+      });
   }
 );
